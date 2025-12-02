4b7dab8e8ff6d5dd96bd1f57811c696e0540dda6
范佳兴 <c1111111c@163.com>
2025-12-02 14:59:58 +0800

    fix:删除冗余代码，修改部分bug

--- ruoyi-ui/src/views/system/user/index.vue
+++ ruoyi-ui/src/views/system/user/index.vue
@@ -425,7 +425,7 @@ export default {
         { key: 0, label: `用户编号`, visible: true },
         { key: 1, label: `用户名称`, visible: true },
         { key: 2, label: `用户昵称`, visible: true },
-        { key: 3, label: `部门/班级`, visible: true },
+        { key: 3, label: `部门/班级`, visible: false },
         { key: 4, label: `手机号码`, visible: true },
         { key: 5, label: `状态`, visible: true },
         { key: 6, label: `创建时间`, visible: true }
